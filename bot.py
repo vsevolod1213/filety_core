2ed28c4a6cef9d15674b2c509f70021eac3aecd6
vsevolod1213 <lvsevolod201@gmail.com>
2025-11-03 01:29:29 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,52 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import os
+from dotenv import load_dotenv
+from aiogram.handlers import MessageHandler
+from transcription import transcription
+from pathlib import Path
+from io import BytesIO
+
+load_dotenv()
+
+TOKEN = os.getenv("api_token")
+bot = Bot(token=TOKEN, default_bot_properties=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp= Dispatcher()
+
+#files_dir = Path("whisper/files")
+
+@dp.message(CommandStart())
+async def send_welcome(message: Message):
+    await message.answer(
+        "Привет! Отправь мне аудиофайл, и я его расшифрую."
+    )
+
+@dp.message(F.document | F.audio | F.voice)
+class TranscribeHandler(MessageHandler):
+    async def handle(self):
+        message = self.event
+        await message.answer("Получил файл, начинаю расшифровку... (это может занять время)")
+        
+        file_obj = message.document or message.audio or message.voice
+        if not file_obj:
+            await message.answer("Не удалось получить файл. Попробуйте еще раз.")
+            return
+        
+        bio = BytesIO()
+        await bot.download(file_obj, destination=bio)
+
+        text = await asyncio.to_thread(transcription, bio)
+
+        await message.answer(f"{text}")
+
+
+async def main():
+    me = await bot.get_me()
+    print(f"Бот запущен: @{me.username} (id: {me.id})")
+    await dp.start_polling(bot)
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ transcription.py
@@ -0,0 +1,25 @@
+from faster_whisper import WhisperModel
+from io import BytesIO
+
+#  "small", "medium", "large-v3"
+model_size = "medium"
+model = WhisperModel(model_size, device="auto")
+
+files_dir = "whisper/files/"
+def transcription(bio: BytesIO):
+
+    bio.seek(0)
+    segments, info = model.transcribe(bio, 
+    task="transcribe",
+    language=None,                   
+    condition_on_previous_text=False,#
+    beam_size=5,
+    vad_filter=True)
+    text = "\n".join(segment.text for segment in segments)   
+
+    return text
+
+    #print(f"Detected language: {info.language} ({info.language_probability:.2f})")
+
+    #for segment in segments:
+      #  print(f"[{segment.start:.2f}s → {segment.end:.2f}s] {segment.text}")
