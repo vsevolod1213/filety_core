e78936e57fcb3f9ba6580e41e85dd7f6411678db
vsevolod1213 <lvsevolod201@gmail.com>
2025-11-07 01:29:52 +0300

    base_structure_for_web

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Filety Transcribe</h1>
+      <p className="text-jopa-700 mt-4">Сервис для транскрипции аудио и видео</p>
+    </main>
+  );
+}
