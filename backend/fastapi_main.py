cdfb7ea47231cfde0674da8df2bb168e6cfd695a
vsevolod1213 <lvsevolod201@gmail.com>
2025-11-13 23:54:27 +0300

    fix_fastapi

--- backend/fastapi_main.py
+++ backend/fastapi_main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import asyncio
-from transcription import which_file
+from backend.transcription import which_file
 
-app = FastAPI(tiyle="Failety API")
+app = FastAPI(title="Failety API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://filety.ru", "http://localhost:3000", "http://127.0.0.1:3000"],
@@ -12,11 +12,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.get("/health")
+@app.get("/health")
 async def health_check():
     return {"status": "ok"}
 
-app.get("/translate")
+@app.get("/translate")
 async def translate_file(file: UploadFile = File(...)):
     content = await file.read()
     try:
