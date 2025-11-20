ef6e48b201970214847f4927269708773e847540
vsevolod1213 <lvsevolod201@gmail.com>
2025-11-20 18:35:40 +0300

    pgadmin -> adminer

--- backend/api/v1/health.py
+++ backend/api/v1/health.py
@@ -1,5 +1,6 @@
 # backend/api/v1/health.py
 from fastapi import APIRouter, Depends
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 from backend.core.deps import get_db
 
@@ -10,7 +11,7 @@ router = APIRouter(prefix="/health", tags=["health"])
 #простой запрос для проверки
 def health_db(db: Session = Depends(get_db)):
     try:
-        db.execute("SELECT 1")
+        db.execute(text("SELECT 1"))
         return {"status": "ok"}
     except Exception as e:
         return {"status": "error", "detail": str(e)}
